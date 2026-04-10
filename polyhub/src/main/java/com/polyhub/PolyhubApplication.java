f14a282f2b6d4517356abf13992b3853d9276668
phantrantien5 <phantrantien5@gmail.com>
2026-04-10 16:57:52 +0000

    mentor6

--- polyhub/src/main/java/com/polyhub/PolyhubApplication.java
+++ polyhub/src/main/java/com/polyhub/PolyhubApplication.java
@@ -2,11 +2,9 @@ package com.polyhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 @EnableAsync // Bật tính năng chạy bất đồng bộ (chủ yếu dùng gửi Email ko bị đơ website)
 public class PolyhubApplication {
 
