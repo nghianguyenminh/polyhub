42bf39fd9be067f1922537d621886d804a6200b3
phantrantien5 <phantrantien5@gmail.com>
2026-04-10 16:33:00 +0000

    mentor4

--- polyhub/src/main/java/com/polyhub/service/impl/UserServiceImpl.java
+++ polyhub/src/main/java/com/polyhub/service/impl/UserServiceImpl.java
@@ -116,4 +116,9 @@ public class UserServiceImpl implements UserService {
     public Optional<User> findById(String id) {
         return userRepository.findById(id);
     }
+
+    @Override
+    public List<User> findByRole(String roleId) {
+        return userRepository.findByRole_Id(roleId);
+    }
 }
