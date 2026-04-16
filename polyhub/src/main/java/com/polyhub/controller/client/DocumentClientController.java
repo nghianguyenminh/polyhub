e5ca4b7bc7a6f1abfda8416ba73011af0eef53ab
phantrantien5 <phantrantien5@gmail.com>
2026-04-16 15:37:35 +0000

    f

--- polyhub/src/main/java/com/polyhub/controller/client/DocumentClientController.java
+++ polyhub/src/main/java/com/polyhub/controller/client/DocumentClientController.java
@@ -6,7 +6,7 @@ import com.polyhub.service.CategoryService;
 import com.polyhub.service.client.DocumentClientService;
 import java.io.IOException;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,11 +20,19 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/documents")
-@RequiredArgsConstructor
 public class DocumentClientController {
 
   private final DocumentClientService documentClientService;
   private final CategoryService categoryService;
+
+  @Autowired
+  public DocumentClientController(
+    DocumentClientService documentClientService,
+    CategoryService categoryService
+  ) {
+    this.documentClientService = documentClientService;
+    this.categoryService = categoryService;
+  }
 
   @GetMapping
   public String showDocumentsPage(
