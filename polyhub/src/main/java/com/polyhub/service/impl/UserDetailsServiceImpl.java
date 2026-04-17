08fea8d3c3722076810f5f3f2cab176aebabd897
phantrantien5 <phantrantien5@gmail.com>
2026-04-17 18:58:49 +0000

    uf

--- polyhub/src/main/java/com/polyhub/service/impl/UserDetailsServiceImpl.java
+++ polyhub/src/main/java/com/polyhub/service/impl/UserDetailsServiceImpl.java
@@ -2,8 +2,8 @@ package com.polyhub.service.impl;
 
 import com.polyhub.entity.User;
 import com.polyhub.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import java.util.Collections;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,12 +11,16 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
             .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
