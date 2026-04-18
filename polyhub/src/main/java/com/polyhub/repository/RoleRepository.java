635855ef75dc8d172933376a8243d3ab8ad3a4b0
phantrantien5 <phantrantien5@gmail.com>
2026-04-18 02:25:46 +0000

    f

--- polyhub/src/main/java/com/polyhub/repository/RoleRepository.java
+++ polyhub/src/main/java/com/polyhub/repository/RoleRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, String> {
+// Sửa String thành Integer để khớp với kiểu ID mới của Role
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     Optional<Role> findByName(String name);
 }
