7ad74e5319ab0bd806313cf4290680d8d2c6465f
phantrantien5 <phantrantien5@gmail.com>
2026-04-20 17:08:24 +0000

    2chge

--- polyhub/src/main/java/com/polyhub/controller/CommentRestController.java
+++ polyhub/src/main/java/com/polyhub/controller/CommentRestController.java
@@ -2,9 +2,7 @@ package com.polyhub.controller;
 
 import com.polyhub.dto.CommentDTO;
 import com.polyhub.dto.CommentRequestDTO;
-import com.polyhub.entity.User;
 import com.polyhub.service.CommentService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
