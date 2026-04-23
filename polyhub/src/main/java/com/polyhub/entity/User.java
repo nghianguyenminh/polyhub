70b7c28f982e5b4ad4405c2990ba93bd298b7c22
Tien2789 <phantrantien5@gmail.com>
2026-04-23 12:56:55 +0700

    mentor dash board

--- polyhub/src/main/java/com/polyhub/entity/User.java
+++ polyhub/src/main/java/com/polyhub/entity/User.java
@@ -1,12 +1,25 @@
 package com.polyhub.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
